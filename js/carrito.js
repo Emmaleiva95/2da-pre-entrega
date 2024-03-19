a4c51f8e29e7b06ee2e9da84aa0359fe4759796b
Emma Leiva <emmaleiva95@gmail.com>
2024-03-19 18:58:36 -0300

    Entrega Final JS

--- js/carrito.js
+++ js/carrito.js
@@ -23,16 +23,54 @@ if(datosCarrito && datosCarrito.length > 0){
     for (const producto of carrito) {
         cuerpoTabla.innerHTML += `
         <tr>
-            <td><img class="img-tabla" src="./img/${producto.imagen}" alt=""></td>
+            <td><img class="img-tabla" src="./assets/img/${producto.imagen}" alt=""></td>
             <td>${producto.titulo}</td>
             <td>$${producto.precio}</td>
-            <td>${producto.cantidad}</td>
+            <td>
+                <input data-idproducto="${producto.id}" class="form-control mb-2 input-cantidad" type="number" value="${producto.cantidad}" min=1 />
+            </td>
             <td>$${producto.subtotal}</td>
             <td>
                 <button class="btn text-danger fw-bold btn-quitar" id="${producto.id}">Quitar</button>
             </td>
         </tr>
         `;
+    }
+
+    let listadoInputs = document.querySelectorAll('.input-cantidad');
+
+    for (const inputCantidad of listadoInputs) {
+        inputCantidad.addEventListener('input', () => {
+            let nuevaCantidad = parseInt(inputCantidad.value);
+
+            let idProducto = inputCantidad.dataset.idproducto;
+            let indiceProducto = carrito.findIndex((element) => { return element.id == idProducto });
+
+            
+            if(nuevaCantidad >= 1 && indiceProducto >= 0){
+                if(nuevaCantidad <= carrito[indiceProducto].stock){
+                    carrito[indiceProducto].cantidad = nuevaCantidad 
+                    let subtotal = carrito[indiceProducto].cantidad * carrito[indiceProducto].precio;
+                    carrito[indiceProducto].subtotal = subtotal; 
+        
+                    inputCantidad.parentNode.nextElementSibling.textContent = `$${subtotal}`;
+        
+                    let total = calcularTotal(carrito);
+                    spanTotal.textContent = total;
+        
+                    localStorage.setItem('datosCarrito', JSON.stringify(carrito));
+                }else{
+                    inputCantidad.value = carrito[indiceProducto].cantidad;
+                    mostrarNotificacion('Ha alcanzado el stock disponible', 'red');
+                }
+                
+                
+            }else{
+                inputCantidad.value = 1;
+            }
+            
+
+        })
     }
 
     let btnQuitar = document.querySelectorAll('.btn-quitar');
@@ -53,33 +91,77 @@ if(datosCarrito && datosCarrito.length > 0){
 
             btn.parentNode.parentNode.remove();
 
+
+
             spanCarrito.textContent = carrito.length;
             spanCarrito.style.display = 'inline';
 
             let total = calcularTotal(carrito);
             spanTotal.textContent = total;
 
+            if(carrito.length == 0){
+                contenedorTablaCarrito.innerHTML = `
+                <div class="text-center">
+                    <p style="width:fit-content;" class="mx-auto bg-info text-white p-4 rounded">Su carrito está vacío, por favor seleccione nuevos productos.</p>
+                    <a class="btn btn-secondary" href="./index.html">Ver productos</a>
+                </div>
+                `;
+                spanCarrito.style.display = 'none';
+            }
+
+
         })
     }
 
+
     const btnFinalizarCompra = document.querySelector('#btnFinalizarCompra');
+    const inputsForm = document.querySelectorAll('.input-form');
+    console.log(inputsForm);
     btnFinalizarCompra.addEventListener('click', (e) => {
         e.preventDefault();
         const ventanaPopup = document.querySelector('.ventana-popup');
         ventanaPopup.style.display = 'flex';
-        localStorage.removeItem('carritoLocal');
-  
-
-        localStorage.clear();
     })
+
+    const btnFormPopUp = document.querySelector('#btnFormPopUp');
+    btnFormPopUp.addEventListener('click', (e) => {
+        e.preventDefault();
+        /* VALIDAR FORM */
+        let msjError = ""
+
+        inputsForm.forEach(input => {
+            console.log(input.value);
+            if(input.value == ''){
+                msjError += `
+                    El campo ${input.id} no puede quedar vacío <br>
+                `;
+            }
+        });
+
+        if(msjError != ''){
+            Swal.fire({
+                title: "Ha ocurrido un error!",
+                html: msjError,
+                icon: "error"
+            });
+        }else{
+           
+            localStorage.removeItem('carritoLocal');
+            localStorage.clear();
+
+            window.location.href = './index.html';
+        }
+    })
+
 
 }else{
     contenedorTablaCarrito.innerHTML = `
-    <div class="text-center">
+    <div class="text-center pb-5 msj-carrito">
     <p style="width:fit-content;" class="mx-auto bg-info text-white p-4 rounded">Su carrito está vacío, por favor seleccione nuevos productos.</p>
     <a class="btn btn-secondary" href="./index.html">Ver productos</a>
     </div>
     `;
 }
+
 
 
--- js/funciones.js
+++ js/funciones.js
@@ -14,7 +14,7 @@ function mostrarNotificacion(mensaje, bgColor){
   
     setTimeout(() => {
         bloqueNotificacion.remove();
-    }, 1000);
+    }, 1500);
 
 
     
@@ -26,4 +26,114 @@ function calcularTotal(arregloCarrito){
         total += producto.subtotal;
     }
     return total;
+}
+
+async function listarProductos(url){
+
+    try{
+        
+    const response = await fetch(url);
+    const data = await response.json();
+
+        
+    // CREAMOS UN ARREGLO VACÍO
+    let listadoProductos = [];
+    // RECORREMOS LA DATA BASE, Y POR CADA OBJETO, CREAMOS UNO NUEVO PERO UTILIZANDO LA CLASE PRODUCTO.
+    for (const producto of data) {
+        const productoObj = new Producto(producto.id, producto.titulo, producto.descripcion, producto.precio, producto.stock, producto.imagen)
+        listadoProductos.push(productoObj);
+    }
+
+    // SELECCIONAR EL CONTENEDOR HTML DONDE QUIERO MOSTRAR LA INFO DE LOS PRODUCTOS.
+    const contenedorProductor = document.querySelector('#contenedorProductos');
+
+    // RECORRO EL ARREGLO DE PRODUCTOS, Y POR CADA PRODUCTO ARMO LA ESTRUCTURA HTML CORRESPONDIENTE DENTRO DEL CONTENEDOR PREVIAMENTE SELECCIONADO.
+    for (const objeto of listadoProductos) {
+        contenedorProductor.innerHTML += `
+        <div class="col-md-4 my-4">
+            <div class="card p-4">
+                <img src="./assets/img/${objeto.imagen}" class="img-fluid" alt="">
+                    <h4>${objeto.titulo}</h4>
+                    <p>${objeto.descripcion}</p>
+                    <span class="d-inline my-4">$${objeto.precio}</span>
+                    <label>Cantidad: </label>
+                    <input class="form-control mb-2 input-cantidad" type="number" value=1 min=1 />
+                    <a href="#" id="${objeto.id}" class="btn btn-success btn-carrito">Añadir al carrito</a>
+            </div>
+        </div>
+    `;
+    }
+
+
+        
+    // SELECCIONO TODOS LOS BOTONES EN UNA CONSTANTE (ARREGLO DE NODOS/OBJETOS HTML)
+    const botonesCarrito = document.querySelectorAll('.btn-carrito');
+
+    // RECORRO TODOS LOS BOTONES Y LE APLICO UN ADDEVENTLISTENER PARA MODIFICAR EL EVENTO CLICK
+    for (const boton of botonesCarrito) {
+        boton.addEventListener('click', (event) => {
+            // PREVENIR O QUITAR LA FUNCIONALIDAD POR DEFECTO DEL ELEMENTO.
+            event.preventDefault();
+        
+            // OBTENGO EL ID DESDE EL BOTON
+            let idProducto = boton.id;
+            // BUSCO EL PRODUCTO DENTRO DEL LISTADO
+            const productoSeleccionado = listadoProductos.find( (element) => { return element.id == idProducto } );
+            // OBTENGO LA CANTIDAD Y CALCULO EL SUBTOTAL
+            let inputCantidad = boton.previousElementSibling;
+            let cantidad = parseInt(inputCantidad.value);
+
+            if(cantidad <= 0){
+                inputCantidad.value = 1;
+                mostrarNotificacion('No puede seleccionar una cantidad menor a 0', 'red');
+            }else if(cantidad > productoSeleccionado.stock){
+                inputCantidad.value = productoSeleccionado.stock;
+                mostrarNotificacion('Ha alcanzado el stock disponible', 'red');
+            }
+            else{
+                let subtotal = productoSeleccionado.precio * cantidad;
+                // VERIFICO SI EL PRODUCTO YA EXISTE EN EL CARRITO
+                let productoExistente = carrito.findIndex( (element) => { return element.id == idProducto });
+        
+                if(productoExistente >= 0){
+                    let nuevaCantidad = carrito[productoExistente].cantidad + cantidad;
+                    if(nuevaCantidad > carrito[productoExistente].stock){
+                        mostrarNotificacion('Ha superado el stock disponible', 'red');
+                    }else{
+                        carrito[productoExistente].cantidad = nuevaCantidad; 
+                        carrito[productoExistente].subtotal += subtotal;
+                    }
+                
+                }else{
+                    // SPREAD -> UNA COPIA Y NO LA REFERENCIA REAL DEL OBJETO.
+                    carrito.push({...productoSeleccionado, cantidad: cantidad, subtotal: subtotal});
+                }
+        
+        
+                localStorage.setItem('datosCarrito', JSON.stringify(carrito));
+            
+        
+                mostrarNotificacion('Producto Agregado', '#198754');
+        
+                spanCarrito.textContent = carrito.length;
+                spanCarrito.style.display = 'inline';
+            }
+
+        
+
+        });
+    }
+        
+    }catch(error){
+        
+        Swal.fire({
+            title: "Ha ocurrido un error!",
+            text: error,
+            icon: "error"
+        });
+    }
+
+
+
+ 
 }
